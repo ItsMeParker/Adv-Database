02c64877d53760c1535e874f91d9ca0af5e7bb06
Connor Parker <Connor2.Parker@live.uwe.ac.uk>
2019-03-16 22:41:06 +0000

    ER in layout as it is in submission document

--- ER.sql
+++ ER.sql
@@ -1,44 +1,35 @@
+CREATE TABLE `Booking` (
+  `Booking_ID : INTEGER` <type>,
+  `Per_or_Tel : BOOLEAN` <type>,
+  `Authorisation : INTEGER` <type>
+);
+
 CREATE TABLE `Payment` (
   `Payment_ID : INTEGER` <type>,
   `* Booking_ID : INTEGER` <type>,
-  `Card_Number : VARCHAR(TBD)` <type>,
-  `Card_Type : VARCHAR(TBD)` <type>,
-  `Amount_Payed : DECIMAL` <type>,
-  `Field` <type>
-);
-
-CREATE TABLE `Booking` (
-  `Booking_ID : INTEGER` <type>,
-  `Booking_Date : TIMESTAMP` <type>,
-  `Payment_ID : INTEGER` <type>,
-  `Per_or_Tel : BOOLEAN` <type>,
-  `Authorisation : INTEGER` <type>,
-  `` <type>
-);
-
-CREATE TABLE `Performance` (
-  `Performace_Date : DATETIME` <type>,
-  `Name : VARCHAR(TBD)` <type>,
-  `` <type>,
-  `` <type>
-);
-
-CREATE TABLE `Booked Seat` (
-  `Booking_ID : INTEGER` <type>,
-  `Performace_Date : DATETIME` <type>,
-  `* Seat_ID : VARCHAR(3)` <type>,
-  `` <type>
+  `Card_Number : VARCHAR(8)` <type>,
+  `Card_Type : VARCHAR(30)` <type>,
+  `Amount_Paid : DECIMAL` <type>
 );
 
 CREATE TABLE `Ticket Category` (
   `Category_ID : INTEGER` <type>,
-  `Price : DECIMAL` <type>,
-  `` <type>
+  `Price : DECIMAL` <type>
 );
 
-CREATE TABLE `All Seats` (
-  `Seat_ID : VARCHAR(3)` <type>,
+CREATE TABLE `Seat` (
   `Performace_Date : DATETIME` <type>,
-  `* Category_ID : INTEGER` <type>,
-  `` <type>
+  `Seat_ID : VARCHAR(3)` <type>,
+  `* Booking_ID : INTEGER` <type>,
+  `* Category_ID : INTEGER` <type>
+);
+
+CREATE TABLE `Seat Booking` (
+  `Booking_ID : INTEGER` <type>,
+  `Performace_Date : DATETIME` <type>
+);
+
+CREATE TABLE `Performance` (
+  `Performace_Date : DATETIME` <type>,
+  `Name : VARCHAR(100)` <type>
 );
