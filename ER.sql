9fdae0c357908127af6b5dde40e5dc8cd0a20710
Connor Parker <Connor2.Parker@live.uwe.ac.uk>
2019-03-16 22:38:54 +0000

    Changes lucid chart ER pretty sure it is wrong

--- ER.sql
+++ ER.sql
@@ -1,31 +1,7 @@
-CREATE TABLE `Customer` (
-  `Customer_ID : INTEGER` <type>,
-  `Field` <type>,
-  `Field` <type>
-);
-
-CREATE TABLE `Performance` (
-  `Performance_ID : INTEGER` <type>,
-  `Performace_Date : TIMESTAMP` <type>,
-  `Name : VARCHAR(TBD)` <type>
-);
-
-CREATE TABLE `Seat Booking` (
-  `Booking_ID : INTEGER` <type>,
-  `Performace_ID : INTEGER` <type>,
-  `Seats_Booked : VARCHAR(100)` <type>,
-  `Category_ID : INTEGER` <type>
-);
-
-CREATE TABLE `Ticket Categories` (
-  `Category_ID : INTEGER` <type>,
-  `Price : DECIMAL` <type>
-);
-
 CREATE TABLE `Payment` (
   `Payment_ID : INTEGER` <type>,
-  `Booking_ID : INTEGER` <type>,
-  `Card_Number : VARCAHR(TBD)` <type>,
+  `* Booking_ID : INTEGER` <type>,
+  `Card_Number : VARCHAR(TBD)` <type>,
   `Card_Type : VARCHAR(TBD)` <type>,
   `Amount_Payed : DECIMAL` <type>,
   `Field` <type>
@@ -36,4 +12,33 @@ CREATE TABLE `Booking` (
   `Booking_Date : TIMESTAMP` <type>,
   `Payment_ID : INTEGER` <type>,
   `Per_or_Tel : BOOLEAN` <type>,
-  `Authorisatio
+  `Authorisation : INTEGER` <type>,
+  `` <type>
+);
+
+CREATE TABLE `Performance` (
+  `Performace_Date : DATETIME` <type>,
+  `Name : VARCHAR(TBD)` <type>,
+  `` <type>,
+  `` <type>
+);
+
+CREATE TABLE `Booked Seat` (
+  `Booking_ID : INTEGER` <type>,
+  `Performace_Date : DATETIME` <type>,
+  `* Seat_ID : VARCHAR(3)` <type>,
+  `` <type>
+);
+
+CREATE TABLE `Ticket Category` (
+  `Category_ID : INTEGER` <type>,
+  `Price : DECIMAL` <type>,
+  `` <type>
+);
+
+CREATE TABLE `All Seats` (
+  `Seat_ID : VARCHAR(3)` <type>,
+  `Performace_Date : DATETIME` <type>,
+  `* Category_ID : INTEGER` <type>,
+  `` <type>
+);
